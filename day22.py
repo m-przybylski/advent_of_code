2409a4dd1e1f634bb980394865ee67582db84438
Maciej Przybylski <m-przybylski@hotmail.com>
2024-12-24 11:16:04 +0100

    day 23

--- day22.py
+++ day22.py
@@ -28,19 +28,19 @@ def partOne():
 def partTwo():
   seq_to_total = {}
 
-  for num in secrets:
-      buyer = [num % 10]
-      for _ in range(2000):
-          num = doMagic(num)
-          buyer.append(num % 10)
-      seen = set()
-      for i in range(len(buyer) - 4):
-          a, b, c, d, e = buyer[i:i + 5]
-          seq = (b - a, c - b, d - c, e - d)
-          if seq in seen: continue
-          seen.add(seq)
-          if seq not in seq_to_total: seq_to_total[seq] = 0
-          seq_to_total[seq] += e
+  for secret in secrets:
+    buyer = [secret % 10]
+    for _ in range(2000):
+      secret = doMagic(secret)
+      buyer.append(secret % 10)
+    seen = set()
+    for i in range(len(buyer) - 4):
+      a, b, c, d, e = buyer[i:i + 5]
+      seq = (b - a, c - b, d - c, e - d)
+      if seq in seen: continue
+      seen.add(seq)
+      if seq not in seq_to_total: seq_to_total[seq] = 0
+      seq_to_total[seq] += e
 
   print(max(seq_to_total.values()))
 

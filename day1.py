b2598ce1a5efb13f9dbc1068a14f09c7bedb59db
Maciej Przybylski <m-przybylski@hotmail.com>
2025-12-01 23:28:20 +0100

    day 01

--- day1.py
+++ day1.py
@@ -1,14 +1,50 @@
 input = "day1"
 
-locations = list(map(list, zip(*[list(map(int, line.split())) for line in open(input, "r").readlines()])))
-for locationIds in locations: locationIds.sort()
+rotations =  list((line[0], int(line[1:len(line)-1])) for line in open(input, "r").readlines())
 
 def partOne():
-  print(sum(abs(left - right)for left, right in zip(*locations)))
+  count = 0
+  current = 50
+  for direction, step in rotations:
+    step = step % 100
+    if direction == 'L':
+      current = current - step
+      if current < 0:
+        current = 100 + current
+    else:
+      current = current + step
+      if current > 99:
+        current = current - 100
+    if current == 0:
+      count += 1
+  print(count)
 
 def partTwo():
-  left, right = locations
-  print(sum(l * right.count(l) for l in left))
+  count = 0
+  current = 50
+  for direction, step in rotations:
+    count += int(step / 100)
+    step = step % 100
+    if direction == 'L':
+      if current == 0:
+        current = 100
+      current = current - step
+      if current < 0:
+        current = 100 + current
+        if current != 0:
+          count += 1
+        print(current, step, 'L')
+    else:
+      current = current + step
+      if current > 99:
+        current = current - 100
+        if current != 0:
+          count += 1
+        print(current, step, 'R')
+    if current == 0:
+      print(current)
+      count += 1
+  print(count)
 
 partOne()
 partTwo()
